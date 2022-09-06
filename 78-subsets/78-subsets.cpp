0a2bf3e24b98f80d5a82e70b0b95d3a4350d3292
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-09-06 14:08:23 +0600

    Time: 3 ms (74.50%), Space: 16.8 MB (8.55%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
 private:
-    void solve(vector<int> nums, vector<int> output,int index, vector<vector<int>>& ans){
+    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
         //base case
         if(index >= nums.size()){
             ans.push_back(output);
             return;
         }
         
+        //recuesive call
         //exclude
         solve(nums,output,index+1,ans);
         
         //include
-        int element = nums[index]; 
+        int element = nums[index];
         output.push_back(element);
         solve(nums,output,index+1,ans);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         vector<vector<int>> ans;
         vector<int> output;
         int index = 0;
