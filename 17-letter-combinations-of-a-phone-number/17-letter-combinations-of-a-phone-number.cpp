8b78e2bd71435f81893fadb52a911a8ec9c349dd
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-09-07 14:26:24 +0600

    Time: 0 ms (100.00%), Space: 6.5 MB (53.89%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,5 +1,5 @@
 class Solution {
-private:
+    private:
     void solve(string digit, string output, int index, vector<string>& ans, string mapping[]){
         //base case
         if(index >= digit.length()){
@@ -7,19 +7,21 @@ private:
             return;
         }
         
-        int number = digit[index] - '0';
-        string value = mapping[number]; //2 = abc
+        int num = digit[index] - '0';
+        string value = mapping[num]; //2 = 'ábc'
         
         for(int i = 0;i<value.length();i++){
             output.push_back(value[i]);
-            //Recursion
-            solve(digit, output, index+1, ans, mapping);
+            
+            //recursive call
+            solve(digit,output,index+1,ans,mapping);
             //backtracking
             output.pop_back();
         }
     }
     
-public:
+    
+    public:
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
         
@@ -28,11 +30,10 @@ public:
             return ans;
         }
         
-        string output = "";
         int index = 0;
-        
+        string output = "";
         string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
-        
+       
         solve(digits,output,index,ans,mapping);
         return ans;
     }
