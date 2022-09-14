93ed849fed3984c0919b6ec2b96c37e4087998b4
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-09-14 15:29:39 +0600

    Time: 8 ms (81.91%), Space: 16.1 MB (41.32%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,43 +1,54 @@
+/* 
+Time Complexity:  O(Exponential) 
+Space Complexity: O(TargetSum) {Recursive Stack Space}
+*/
+
+/* The idea of this question is to return all such combinations whose individual sum is equal to Target
+We use the same logic as generating Combinations - only difference is that we calculate the sum of each subset every time.
+We can use an element as many times as we want
+Our given vector arr[] contains only Unique Subsets that's for sure
+*/
+
 class Solution {
 public:
-    vector<vector<int>> ans;
-    
-    void solve(int i,vector<int>& arr, vector<int>& temp, int target){
-        
+    void findCombination(int i, vector<int>& arr, int target, int sumNow, vector<int>& output, vector<vector<int>>& ans){
         //base case
-        if(target == 0){
-            ans.push_back(temp);
+        
+        // If our sumTillNow is equal to target, we have reached a Valid Combination
+        if(sumNow == target){
+            ans.push_back(output);
             return;
         }
         
-        if(target < 0){
-            return;
-        }
-        if(i == arr.size()){
-            return;
-        }
+        // If our sumTillNow exceeds target, there's no point in proceeding further(optimization)
+        if(sumNow > target) return;
+        
+         // If we have reached the end of arr[], we cannot go further as we don't have anymore elements
+        if(i >= arr.size()) return;
         
         
-        //recursive calls
+        
         //exclude
-        solve(i+1,arr,temp,target);
-        
+        //recursive call
+        findCombination(i+1, arr, target, sumNow, output, ans);
         
         //include
-        temp.push_back(arr[i]);
-        solve(i,arr,temp,target-arr[i]);
+        sumNow += arr[i];
+        output.push_back(arr[i]);
+        //won't update i because we can take one element unlimited times
+        findCombination(i, arr, target, sumNow, output, ans); 
         //backtracking
-        temp.pop_back();
+        sumNow -= arr[i];
+        output.pop_back();
     }
     
-    
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-       //ans.clear();
-        
-        vector<int> temp;
-        
-        solve(0,arr,temp,target);
-        
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int sumNow = 0;
+        sort(candidates.begin(),candidates.end());
+        findCombination(0, candidates, target, sumNow, output, ans);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
