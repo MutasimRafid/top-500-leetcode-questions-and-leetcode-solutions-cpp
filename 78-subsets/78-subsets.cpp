1b86a90845442a5b8aee2ac4507d583f2f894b99
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-09-27 14:08:30 +0600

    Time: 15 ms (5.60%), Space: 16.7 MB (18.05%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,4 +1,5 @@
 class Solution {
+//we will use take/not take algo
 private:
     void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
         //base case
@@ -7,20 +8,28 @@ private:
             return;
         }
         
-        //recuesive call
         //exclude
-        solve(nums,output,index+1,ans);
+        solve(nums,output,index+1,ans); //recursive call
+        
         
         //include
         int element = nums[index];
         output.push_back(element);
-        solve(nums,output,index+1,ans);
+        solve(nums,output,index+1,ans); //recursive call
+        //in the parameter of solve function, if we take ...vector<int> output.. the address 
+        //then we need to pop back. backtrack. as we are using only 1 vector
+        //but here i am not using address. so in each recursive call a new copy of a vector will be created
+        //so i don't need to backtrack here
+        //output.pop_back(); 
+        
     }
+    
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> output;
         int index = 0;
+        
         solve(nums,output,index,ans);
         return ans;
     }
