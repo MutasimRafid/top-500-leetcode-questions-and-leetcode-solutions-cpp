bb6e865642b4771c3c36ef4843ef0339860f3625
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-10-01 01:12:56 +0600

    Time: 14 ms (20.68%), Space: 7.4 MB (89.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,32 +1,34 @@
 class Solution {
 private:
-    void solve(int i, vector<int>& output, vector<int>& nums, vector<vector<int>>& ans){
+    void solve(int i, vector<int>& output, vector<vector<int>>& ans, vector<int>& nums){
         //base case
         if(i == nums.size()){
             ans.push_back(output);
             return;
         }
         
-        
         //include
         output.push_back(nums[i]);
-        //recursive call]
-        solve(i+1,output,nums,ans);
+        //recursive call
+        solve(i+1,output,ans,nums);
         //backtracking
         output.pop_back();
+        
         
         //stop same element in ans like: [[1,2,2],[1,2,2]]
         //you will take [1,2,2] only once
         //to avoid duplicate element in ans, while excluding will avoid same digit.
+        //
         //test case: 1,2,2 ...here will avoid the 1st 2 which is at i = 1, 
-        //there will be no exclude recursion call for i = 1 
+        //there will be no exclude recursion call for i = 2, i will be 3
+        //see note khata for better explanation
         while(i<nums.size() - 1 && nums[i] == (nums[i+1]) ){
             i++;
         }
         
-        //exclude
-        solve(i+1,output,nums,ans);
         
+        //exclude
+        solve(i+1,output,ans,nums);
         
     }
     
@@ -36,8 +38,7 @@ public:
         vector<vector<int>> ans;
         vector<int> output;
         
-        solve(0,output,nums,ans);
-        
+        solve(0,output,ans,nums);
         
         sort(ans.begin(),ans.end());
         return ans;
