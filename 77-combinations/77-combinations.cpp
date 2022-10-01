749da17a9ca489ad78a19ce3be09508522dd7ea7
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-10-01 19:25:03 +0600

    Time: 15 ms (93.86%), Space: 17.9 MB (37.34%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -9,25 +9,21 @@ We should always remember - Relative order matters in Combination just like Subs
 Time Complexity  : O(2^N)
 Space Complexity : O(N)
 */
+
 class Solution {
 private:
-    void solve(int i, int n, int k, vector<int>& output, vector<vector<int>>& ans){
-        //base condition
+    void solve(int i,int n, int k, vector<int>& output, vector<vector<int>>& ans){
+        //base case
         if(k == 0){
             ans.push_back(output);
             return;
         }
         
+        if(i>n) return;
+        
         // If we don't have enough elements to make our subset, we shouldn't proceed further
         //this base case is for optimization only, not mandatory
-        if(k>n-i+1){
-            return;
-        }
-        
-        //if index is greater than n
-        if(i > n) return;
-        
-        
+        if(k > n-i+1) return; //optimization
         
         
         //include
@@ -37,18 +33,17 @@ private:
         //backtrack
         output.pop_back();
         
-        
         //exclude
-         solve(i+1,n,k,output,ans);
+        solve(i+1,n,k,output,ans);
+        
     }
-    
     
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> output;
         
-        //we are passing 1 to avoid zero, as because in question it is stated that range 1 to n
+        ////we are passing 1 to avoid zero, as because in question it is stated that range 1 to n
         solve(1,n,k,output,ans);
         return ans;
     }
