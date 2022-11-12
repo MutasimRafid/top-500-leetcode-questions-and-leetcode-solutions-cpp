b53ddafda23df52c597895fba9cd54e8a4ed5793
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-11-12 20:51:20 +0600

    Time: 253 ms (35.04%), Space: 49.9 MB (36.36%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,5 +1,6 @@
 class MyHashSet {
 public:
+    /*
     //solution1: only valid for 10^6 dataset
     //As you can only create vector till 10^6
     vector<int> mp;
@@ -19,6 +20,44 @@ public:
     
     bool contains(int key) {
         return mp[key];
+    }
+    */
+    
+     //solution2: Using chaining technique: Linked list
+    
+    vector<list<int>> m;
+    int siz;
+    MyHashSet() {
+       siz = 10000;
+       m.resize(siz); 
+    }
+    
+    int hash(int key){
+        return key%siz;
+    }
+    
+    list<int> :: iterator search(int key){
+        int i = hash(key); //index of the vector(there is a linked list in every particular index of vector)
+        return find(m[i].begin(),m[i].end(),key); //returning iterator
+    }
+    
+    void add(int key) {
+        if(contains(key)) return;
+        int i = hash(key);
+        m[i].push_back(key);
+    }
+    
+    void remove(int key) {
+       if(!contains(key))return;
+        int i = hash(key);
+        m[i].erase(search(key)); //search(key) will return iterator of the linked list
+         
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        if(search(key) != m[i].end()) return true;
+        else return false;
     }
 };
 
