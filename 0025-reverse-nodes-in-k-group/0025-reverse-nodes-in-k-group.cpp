304bf577b9c2fdce88e2e9faac0057239ff9962e
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-11-16 23:28:28 +0600

    Time: 33 ms (53.63%), Space: 11.6 MB (30.38%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -9,7 +9,10 @@
  * };
  */
 class Solution {
+    
 public:
+    /*
+    //solution 1: Recursive solution
     void reverse(ListNode* s,ListNode* e){
         ListNode *p = NULL,*c = s,*n=s->next;
         while(p!=e){
@@ -21,7 +24,7 @@ public:
     }
     
     
-    //solution 1: Recursive solution
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
        if(head == NULL || head->next == NULL || k==1) return head;
         ListNode *s = head, *e = head;
@@ -35,5 +38,42 @@ public:
         reverse(s,e);
         s->next = nextHead;
         return e;
+    }
+    */
+    
+    
+    //solution 2: O(1) space solution .. interview follow up.. iterative solution
+    void reverse(ListNode* s,ListNode* e){
+        ListNode *p = NULL,*c = s,*n=s->next;
+        while(p!=e){
+            c->next = p;
+            p = c;
+            c = n;
+            if(n!=NULL)n=n->next;
+        }
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+       if(head == NULL || head->next == NULL || k==1) return head;
+        ListNode *dummy = new ListNode(-1);
+        dummy->next=head;
+        ListNode *beforeStart = dummy, *e = head;
+        int i = 0;
+        while(e!=NULL){
+            i++;
+            if(i%k == 0){
+                //reversal
+                ListNode *s = beforeStart->next, *temp = e->next;
+                reverse(s,e);
+                beforeStart->next = e;
+                s->next = temp;
+                beforeStart = s;
+                e = temp;
+            }
+            else{
+                e = e->next;
+            }
+        }
+        return dummy->next;
     }
 };
