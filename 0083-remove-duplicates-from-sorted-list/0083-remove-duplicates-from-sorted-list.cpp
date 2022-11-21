496978d35cf9b716c4533ae4f4d1c7737b79cbf4
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-11-21 14:54:40 +0600

    Time: 32 ms (6.01%), Space: 11.6 MB (37.99%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -10,7 +10,11 @@
  */
 class Solution {
 public:
-    //Solution 1: Recursive solution
+    
+    //Solution 1: Recursive solution:
+    //Recursion TC - O(N)
+    //Recursion SC - O(N) because function is called n times in worst case
+    /*
     ListNode* deleteDuplicates(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
         //recursive call
@@ -20,5 +24,26 @@ public:
             head->next = newHead;
             return head;
         }
+    }
+    */
+    
+    //solution 2: iterative solution: 
+    //Iteration TC - O(N)
+    //Iteration SC - O(1)
+
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        while(temp->next != NULL){
+            if(temp->val == temp->next->val){
+                ListNode*del = temp->next;
+                temp->next = del->next; //skipping duplicate value
+                delete del;
+            }
+            else{
+                temp = temp->next;
+            }
+        }
+        return head;
     }
 };
