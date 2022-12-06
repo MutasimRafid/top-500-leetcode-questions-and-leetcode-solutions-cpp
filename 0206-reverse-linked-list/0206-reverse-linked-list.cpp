a0d94c13f83814c30db1ee486375983300453906
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-12-06 22:11:17 +0600

    Time: 11 ms (64.56%), Space: 8.3 MB (40.18%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,35 +10,18 @@
  */
 class Solution {
 public:
-   //solution 1: iterative method
-    /*
     ListNode* reverseList(ListNode* head) {
-        //solution iterative method
         if(head == NULL) return NULL;
-        ListNode *p = NULL, *c = head, *n = head->next;
         
-        while(c != NULL){
+        ListNode *p = NULL,*c = head,*n = head->next;
+        
+        while(c!=NULL){
             c->next = p;
+            
             p = c;
             c = n;
             if(n!=NULL) n = n->next;
         }
         return p;
-    }
-    */
-    
-    
-    //solution 2: Recursive method
-    ListNode* reverseList(ListNode* head) {
-        //Base case
-        if(head==NULL || head->next==NULL){
-           return head;
-        }
-        
-        ListNode* rest_head = reverseList(head->next);
-        ListNode* rest_tail = head->next;
-        rest_tail->next = head;
-        head->next = NULL;
-        return rest_head;
     }
 };
