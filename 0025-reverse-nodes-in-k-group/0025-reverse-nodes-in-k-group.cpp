4f33c8f866c5c869197de615a314af15fdaccd59
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2022-12-08 14:23:27 +0600

    Time: 22 ms (76.54%), Space: 11.5 MB (73.32%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -9,19 +9,24 @@
  * };
  */
 class Solution {
+    
 public:
-    void reverse(ListNode* s, ListNode* e){
-        ListNode*p = NULL,*c = s, *n = s->next;
+    /*
+    //solution 1: Recursive solution
+    void reverse(ListNode* s,ListNode* e){
+        ListNode *p = NULL,*c = s,*n=s->next;
         while(p!=e){
             c->next = p;
-             p = c;
+            p = c;
             c = n;
-            if(n!=NULL) n = n->next;
+            if(n!=NULL)n=n->next;
         }
     }
     
+    
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL) return head;
+       if(head == NULL || head->next == NULL || k==1) return head;
         ListNode *s = head, *e = head;
         int inc = k-1;
         while(inc--){
@@ -29,9 +34,46 @@ public:
             if(e == NULL) return head;
         }
         
-        ListNode* nextHead = reverseKGroup(e->next,k);
+        ListNode* nextHead = reverseKGroup(e->next,k); //recursive call
         reverse(s,e);
         s->next = nextHead;
         return e;
+    }
+    */
+    
+    
+    //solution 2: O(1) space solution .. interview follow up.. iterative solution
+    void reverse(ListNode* s,ListNode* e){
+        ListNode *p = NULL,*c = s,*n=s->next;
+        while(p!=e){
+            c->next = p;
+            p = c;
+            c = n;
+            if(n!=NULL)n=n->next;
+        }
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+       if(head == NULL || head->next == NULL || k==1) return head;
+        ListNode *dummy = new ListNode(-1);
+        dummy->next=head;
+        ListNode *beforeStart = dummy, *e = head;
+        int i = 0;
+        while(e!=NULL){
+            i++;
+            if(i%k == 0){
+                //reversal
+                ListNode *s = beforeStart->next, *temp = e->next;
+                reverse(s,e);
+                beforeStart->next = e;
+                s->next = temp;
+                beforeStart = s;
+                e = temp;
+            }
+            else{
+                e = e->next;
+            }
+        }
+        return dummy->next;
     }
 };
