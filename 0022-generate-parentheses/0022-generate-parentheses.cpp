4147f2d358bd06ba7306a9a043d17d5500d9a580
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-01-04 16:09:51 +0600

    Time: 15 ms (15.62%), Space: 13.5 MB (64.34%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(vector<string> &v, int n, int oc, int cc, string s){
+        //base case
+        if(oc == n && cc == n){
+            v.push_back(s);
+            return;
+        }
+        
+        if(oc<n){
+            solve(v, n, oc+1, cc, s+"(");
+        }
+        if(cc<oc){
+            solve(v, n, oc, cc+1, s+")");
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        int oc = 0,cc = 0;
+        solve(v,n,oc,cc,"");
+        return v;
+    }
+};
