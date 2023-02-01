ca2a9735b295c8c98bda83b28f5daebcab560176
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-02-01 12:11:24 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (53.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,22 +6,28 @@ public:
         for(int i = 0;i<s.length();i++){
             char ch = s[i];
             
+            // if opening bracket, stack push
+            //if closing bracket, stack top check and pop
             
             if(ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
             }
             else{
+                //for closing bracket
                 if(!st.empty()){
                     char top = st.top();
-                    if(ch == ')' && top == '(' || ch == '}' && top == '{' || ch == ']' && top == '['){
+                    //if(matches(top,ch))
+                    if((ch == ')' && top == '(') || (ch == '}' && top == '{') ||
+                      (ch == ']' && top == '[') ){
                         st.pop();
                     }
-                    else{
-                        return false; //the string is not balanced
+                    else{    //the brackets are not balanced
+                        return false; 
                     }
                 }
-                else{
-                    return false; //the stack is empty and we don't find a closing bracket 
+                
+                else{   //stack is empty and we find a closing bracket in string
+                    return false;
                 }
             }
         }
