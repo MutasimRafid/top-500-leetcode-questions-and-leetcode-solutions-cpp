e832dfa79e1d870064586a3ff982752f0cf82339
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-02-02 13:01:14 +0600

    Time: 29 ms (45.25%), Space: 16.4 MB (62.14%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,6 +1,5 @@
 class MinStack {
 public:
-    
     //Approach 1: use pairs inside a stack
     //TC: O(1).. O(2N)
     /*
@@ -26,13 +25,14 @@ public:
     return st.top().second;
   }
     */
+    
+    
     //Approach 2: using stack but using modified value
     //TC: O(1)... SC: O(N)
-    stack <long long> st;
+    stack<long long> st;
     long long mini;
     
     MinStack() {
-        while(st.empty() == false) st.pop();
         mini = INT_MAX;
     }
     
@@ -43,11 +43,11 @@ public:
             st.push(val);
         }
         else{
-            if(val<mini){
-                st.push(2*val*1LL-mini); //modified value
+            if(val < mini){
+                st.push(2 * val * 1LL - mini); //modified value
                 mini = val;
             }
-            else{
+            else{ //val>mini
                 st.push(val);
             }
         }
@@ -55,20 +55,20 @@ public:
     
     void pop() {
         if(st.empty()) return;
-        long long elem = st.top();
+        long long el = st.top();
         st.pop();
-        if(elem<mini){
-            mini = 2*mini - elem; 
+        
+        if(el<mini){
+            mini = 2 * mini - el; //modified value
         }
     }
     
     int top() {
         if(st.empty()) return -1;
-        long long elem = st.top();
-        if(elem<mini){
-            return mini;
-        }
-        return elem;
+        
+        long long el = st.top();
+        if(el<mini) return mini;
+        return el;
     }
     
     int getMin() {
