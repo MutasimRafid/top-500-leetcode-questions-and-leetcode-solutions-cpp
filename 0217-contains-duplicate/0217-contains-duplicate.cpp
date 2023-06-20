a90b9b424679b48bbcd9f9ee013772feb9883b46
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-06-20 18:50:02 +0600

    Time: 194 ms (21.40%), Space: 70.8 MB (30.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,24 @@
-
 class Solution {
 public:
+    
     //solution 1: using nested loop ..TC: O(n^2)...SC: O(1)
     //solution 2: using sorting... TC: O(nlogn)..SC: O(1)
     //solution 3: using map/array to count frequency .. TC: O(logn)  SC:O(n)
+    
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> countMap;
+       map<int,int> countMap;
         
         for(int i = 0;i<nums.size();i++){
-            //if the element appears 2nd time, in map nums[i] is already 1(first time increment when found)
+            
+            //if the element appears 2nd time
+            //if countMap[nums[i]] is already 1 (not zero)
+            
             if(countMap[nums[i]]){
                 return true;
             }
             
             //if the element appears first time
+            //hashing
             countMap[nums[i]]++;
         }
         return false;
