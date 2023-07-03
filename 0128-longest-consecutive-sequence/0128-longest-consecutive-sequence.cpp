4bcbf77e74b6d0ee055069cbb802aecb7a166ec5
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-07-03 23:41:22 +0600

    Time: 2400 ms (5.01%), Space: 68.8 MB (20.22%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        //1st approach: use sorting 
+        //TC: nlogn
+        
+        //2nd approach: use set
+        //TC: O(n)+O(n)+O(n) = 3 O(n) = O(n)
+        //sc: O(n)
+        
+        set<int> s;
+        //insert all elements into set
+        for(int num: nums){
+            s.insert(num);
+        }
+        
+        int longestStreak = 0;
+        int currNum = 0;
+        int currStreak = 0;
+        
+        for(int num: nums){
+            //if previous element does not exist
+            if(!s.count(num-1)){
+                currNum = num;
+                currStreak = 1;
+                
+                //now i have the minimum number in the streak
+                //eg: given data: 73,72,103,102,100,101
+                //eg: i have 100. now i will check for 101,102,103....
+                while(s.count(currNum+1)){
+                    currNum += 1;
+                    currStreak += 1;
+                }
+            }
+            
+            longestStreak = max(longestStreak,currStreak);
+        }
+        
+        return longestStreak;
+    }
+};
