716c6e87046e1cd3e18c8d4a42567fa823c21e01
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-07-11 13:45:03 +0600

    Time: 3 ms (91.55%), Space: 8 MB (6.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,17 +4,19 @@ public:
         string other = s;
         reverse(other.begin(),other.end());
         return (other == s);
+        
     }
     
     
     bool isPalindrome(string s) {
         string s2 = "";
         for(auto i: s){
-            if(i>='a' && i<='z') s2 += (char)i;
-            else if(i>='A' && i<='Z') s2 += (char)(i-'A'+'a');
-            else if(i>='0' && i<='9') s2 += (char)i;
+            if(i >= 'a' && i<= 'z') s2 += (char)i;
+            else if(i >= 'A' && i<= 'Z') s2 += (char)(i-'A'+'a');
+            else if(i >= '0' && i<= '9') s2 += (char)i;
         }
         
         return palindromeCheck(s2);
     }
+    
 };
