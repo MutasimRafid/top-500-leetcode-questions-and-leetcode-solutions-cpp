3998e3233b478e47adf817b71bb0d68700bd0c54
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2023-07-25 22:33:57 +0600

    Time: 1546 ms (14.31%), Space: 191.7 MB (6.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -60,27 +60,33 @@ public:
     //using 2 pointer approach
     vector<vector<int>> threeSum(vector<int>& nums) {
         int target = 0;
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
         set<vector<int>> s;
         vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
+        
+        for(int i = 0;i<nums.size();i++){
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
+                if(sum == target){
+                    s.insert({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                } else if (sum < target) {
+                }
+                else if(sum < target){
                     j++;
-                } else {
+                }
+                else{
                     k--;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
+        
+        for(auto it: s){
+            output.push_back(it);
+        }
+        
         return output;
     }
     
