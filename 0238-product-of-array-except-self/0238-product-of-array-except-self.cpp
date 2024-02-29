1cac8dd16e1325b9864e70dd2113abb1cd3168e7
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-02-29 13:28:26 +0600

    Time: 21 ms (28.26%), Space: 27.2 MB (29.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    //solution 1: using nested loop: TC:O(n)^2
-    //solution 2: using division TC: O(n) .. SC:O(1)
-    //solution 3: Using 2 extra array.left product array and right product array... TC:O(n).. SC:O(n)
-    
-    //Most optimized according to follow up question.
-     //solution 4: Use output array as leftProduct array and a variable as right product array.
-     // then convert output array as answer array (inplace)
-    //TC:O(n)  ..SC: O(1) output array is not considered as extra space according to question
-    
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> output;
+        //Approach 1: Nested loop. TC: O(n^2)
+        //Approach 2: use 2 array as left[] and right[]  cumulative.. TC: O(n).. SC: O(2n)
+        
+        // Answers the follow up question
+        //Approach 3: Use 1 Array as left[] and use variable product as right[] .. TC: O(n).. SC: O(1)
+        
+       int n = nums.size();
+       vector<int> output;
         //edge case
         if(n<1){
             return output;
         }
         
-        int product = 1; //here using product as normal variable
-        //using output array as left product array
+        int product = 1;
+        //left[]
         for(int i = 0;i<n;i++){
-            product *= nums[i];
-            output.push_back(product);
+            product = product * nums[i];
+            output.push_back(product); //left[i]
         }
         
-        //traverse from right and update output array
-        product = 1; //now using product as right product array
+        //traverse from right and output output array
+        product = 1;
         for(int i = n-1;i>0;i--){
             output[i] = output[i-1] * product;
-            product *= nums[i];
+            product = product * nums[i];
+            
         }
-        output[0] = product; //default value. 
+        output[0] = product;
         
         return output;
         
