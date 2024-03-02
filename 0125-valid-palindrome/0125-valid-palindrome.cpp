17afe2614be8e747dc02d6fe3e4ec02885d9ca17
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-03-02 21:06:30 +0600

    Time: 3 ms (90.71%), Space: 9.5 MB (8.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    bool palindromeCheck(string s){
-        string other = s;
-        reverse(other.begin(),other.end());
-        return (other == s);
-        
+    /*
+        Algo:
+        1. For palindrome check there is palindrome function.
+        2. Palindrome function: string s2 = reverse(s)... loop: if s1 == s2 then yes
+        3. now free space + uppercase letter. make all lowercase and omit 
+        using ascii value.
+        4. then compare using palindrome function
+            */
+    
+    bool isPal(string s){
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        if(s2==s){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
     
     bool isPalindrome(string s) {
-        string s2 = "";
+        
+        string s3 = "";
+        
         for(auto i: s){
-            if(i >= 'a' && i<= 'z') s2 += (char)i;
-            else if(i >= 'A' && i<= 'Z') s2 += (char)(i-'A'+'a');
-            else if(i >= '0' && i<= '9') s2 += (char)i;
+            if(i >= 'a' && i<= 'z') s3+=(char)i;
+            else if(i >= 'A' && i<= 'Z') s3+=(char)(i-'A'+'a');
+            else if(i >= '0' && i<= '9') s3+=(char)i;
         }
         
-        return palindromeCheck(s2);
+        return isPal(s3);
+        
+        
     }
-    
 };
