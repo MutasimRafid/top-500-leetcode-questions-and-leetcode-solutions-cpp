075dc1c1957de3ac17880d110b2c3a295298d18b
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-03-03 22:14:15 +0600

    Time: 11 ms (40.74%), Space: 18 MB (42.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    //solution 1: nested loop. TC: O(n)^2
-    //solution 2: As input is sorted. Use binary search. TC: nlogn
-    //solution 3: As input is sorted. Use 2 pointer approach. TC: O(n) 
     vector<int> twoSum(vector<int>& numbers, int target) {
+        //Approach 1: using nested loop .. TC: O(n^2)
+        //Approach 2: use binary search
+        //Approach 3: using two pointer approach TC: O(n).. SC:O(1)
+        
         int l = 0;
         int r = numbers.size()-1;
         
-        vector<int> ans;
-        
         while(l<r){
-            if(numbers[l]+numbers[r] == target){
-                ans.push_back(l+1);
-                ans.push_back(r+1);
-                
-                return ans;
-                
-                //return {l+1,r+1};        
-            }
+            if((numbers[l]+numbers[r]) == target){
+                return {l+1,r+1};
+            } 
             else if(numbers[l]+numbers[r] > target){
                 r--;
             }
@@ -25,6 +19,7 @@ public:
                 l++;
             }
         }
-        return ans;
+        
+        return {};
     }
 };
