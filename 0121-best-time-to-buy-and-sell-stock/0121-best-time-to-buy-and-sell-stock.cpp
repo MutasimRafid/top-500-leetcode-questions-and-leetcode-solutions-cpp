be4597746eb6dc1e0917d5edc388004be8564ec7
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-03-07 12:25:05 +0600

    Time: 74 ms (94.80%), Space: 95.7 MB (54.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    //solution 1: nested loop . TC: O(n)^2
-    //solution 2: using min and only 1 loop
-    //TC: O(n) SC: O(1)
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
-        int currProfit = 0;
+       //Approach 1: use nested loop... TC: O(n^2)..SC: O(1)
+        //Approach 2: Use sliding window .. TC: O(n).. SC: O(1)
+        
+        int maxPro = 0;
         int minPrice = INT_MAX;
         
         for(int i = 0;i<prices.size();i++){
             minPrice = min(minPrice,prices[i]);
-            currProfit = prices[i] - minPrice;
-            profit = max(profit,currProfit);
+            maxPro = max(maxPro,prices[i]-minPrice);
         }
-        return profit;
+        
+        return maxPro;
     }
 };
