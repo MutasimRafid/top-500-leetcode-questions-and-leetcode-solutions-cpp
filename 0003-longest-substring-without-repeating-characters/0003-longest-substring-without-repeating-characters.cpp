6816fe7dfb4e77182dcfdd0452d7c4c6c1457d93
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-03-09 12:35:48 +0600

    Time: 3 ms (96.12%), Space: 10 MB (74.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    //using sliding window technique, also using hashmap. Here using frequency array as hashmap
-    //TC: O(n)
-    //SC: O(n)
 public:
     int lengthOfLongestSubstring(string s) {
+        //Approach 1: Using Sliding window and hashmap .. TC:O(n).. SC:O(1)
+        
         int count[256] = {0};
-        int l = 0;
-        int r = 0;
-        int ans = 0;
+        int l = 0;//starting index of the window
+        int r = 0;//ending index of the window
+        int ans = 0; //length of the longest substring without repeating chracters
         
         while(r<s.size()){
-            count[s[r]]++;
+            count[s[r]]++; //adding frequency
             
-            while(count[s[r]] > 1 ){
-               count[s[l]]--;
-                l++;
+            //if there is a repeating chracter in the window
+            while(count[s[r]]>1){
+                count[s[l]]--;  //remove frequency from count
+                l++; //shorten the window
             }
             
-            
-            ans = max(ans,r-l+1); //1 based indexing
+            ans = max(ans,r-l+1);
             r++;
         }
         return ans;
