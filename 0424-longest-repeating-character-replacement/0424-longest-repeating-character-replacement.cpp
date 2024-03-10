313a5e65cbddd3befdc6681f6d3fdeb5c720bfbf
Mutasim Rafid <83448718+MutasimRafid@users.noreply.github.com>
2024-03-10 22:33:31 +0600

    Time: 12 ms (41.25%), Space: 9.2 MB (48.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,23 @@
 class Solution {
-    //TC: O(n)
-    //SC: O(26) = O(1)
-    //using two pointer approach
-    //code from leetcode  discussion
-    //watch video tutorial and code in the comment of youtube
 public:
-    
     int characterReplacement(string s, int k) {
-        int n = s.length(),res = 0;
-        int start = 0,end = 0,maxFreq = 0;
-        map<int,int> mp; //frequency map of A , B
-        for(end = 0;end<n;end++){
-            mp[s[end]]++;
-            maxFreq = max(maxFreq,mp[s[end]]);
+        int n = s.length(), res = 0;
+        int start = 0, end = 0, maxFreq = 0;
+        map<int, int> freq;
+        
+        for(end=0; end<n; end++){
+            freq[s[end]]++;
+            maxFreq = max(maxFreq, freq[s[end]]);
             
-            //eg: AABABBA
-            //less Occuring Frequency value(frequency value of B) till now = sliding window length((end-start)+1)  - maxFreq(frequency of A)
-            //this value above > k ... that means we have used all k value
-            //now we need to move start pointer and change the window and form a new substring
-            
+            // len-maxFreq <= k, we have to find if the substring has atmost k different characters which we alter to make all the characters of the string equal
             while(end-start+1 - maxFreq > k){
-                mp[s[start]]--;
+                freq[s[start]]--;
                 start++;
             }
-            res = max(res,end-start+1);
+            
+            res = max(res, end-start+1);
         }
+        
         return res;
     }
 };
